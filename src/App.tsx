da67c909b6a93ec223876aa84597f1e55c0b3379
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-15 08:30:46 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import { ethers } from 'ethers';
+
 import './App.css';
 
-function App() {
+async function App() {
+  let provider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/TmZStpJR5M1MgSupnAyPBGYUDAadhaeo', 1)
+  const address = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+  const ABI:any = ["event Mint(address indexed to, uint256 amount);",
+  "event Burn(address indexed burner, uint256 value);"]
+  const contract = new ethers.Contract(address, ABI, provider)
+
+  const mintTransaction = await contract.queryFilter( 'Mint', 6766284, 'latest')
+  console.log(mintTransaction)
+  useEffect(() =>{
+    console.log(contract)
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
