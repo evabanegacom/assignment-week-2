5319cc9658dc06cd7e082f2a42df4fd490290e6a
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-09-11 07:13:26 +0100

    added semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ function App() {
   const [mints, setMints ] = useState<any>([]);
   const [burns, setBurns ] = useState<any>([]);
   let provider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/TmZStpJR5M1MgSupnAyPBGYUDAadhaeo', 1)
-  const address = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+  const address = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599';
   const ABI:any = ["event Mint(address indexed to, uint256 amount);",
   "event Burn(address indexed burner, uint256 value);"]
   const contract = new ethers.Contract(address, ABI, provider)
   const mint:any = 'Mint'
-  const burn:any = 'Burn'
+  const burn:any = 'Burn';
   const getContracts = async () => {
     const mintTransaction:any = await contract.queryFilter( mint, 6766284, 'latest')
     setMints(mintTransaction)
